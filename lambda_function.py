4bd644e91f5e299218fe291cdbaeb262e5df4c22
Dylan Myers <dylmye-github-signed@opayq.com>
2021-08-18 02:35:35 +0100

    working

--- lambda_function.py
+++ lambda_function.py
@@ -1,55 +1,36 @@
 import json
 import requests
+import base64
+from bs4 import BeautifulSoup, Tag
+
 from env import DISCORD_WEBHOOK_URL, DISCORD_ROLE_ID
 
-# take a twitch event and publish a discord message
 def lambda_handler(event, context):
     return_obj = {
         "statusCode": 200,
-        "body": json.dumps({ "executed": False, "rqid": context["aws_request_id"] })
+        "body": json.dumps({ "executed": False, "rqid": context.aws_request_id })
     }
 
-    # handle different data shape when using
-    # aws api gateway vs directly using lambda
-    data = event
-    if ("stageVariables" in data):
-        data = json.loads(event["body"])
+    soup = BeautifulSoup(event["body"])
+    entry = soup.entry
 
-    if ("subscription" not in data or data["subscription"]["status"] != "enabled" or data["subscription"]["type"] != "stream.online"):
-        return_obj["body"] = json.dumps({ "executed": False, "error": "no subscription in request, or subscription type is incorrect.", "debug_event_obj": data, "rqid": context["aws_request_id"] })
+    if not entry:
+        return_obj["body"] = json.dumps({ "executed": False, "rqid": context.aws_request_id, "error": "No entry found" })
         return return_obj
 
-    mention_str = ('@everyone, ' if DISCORD_ROLE_ID == 'everyone' else '<@&' + DISCORD_ROLE_ID + '>, ')
-    # cache_buster = str(datetime.utcnow().isoformat(timespec='minutes')).replace('-', '').replace(':', '')
+    title = entry.title
+    link = entry.find("link", rel="alternate")
+    channel = entry.author.id
+
+    return_obj["body"] = json.dumps({ "executed": False, "rqid": context.aws_request_id, "title": title.string })
+
+    mention_str = ('@....everyone, ' if DISCORD_ROLE_ID == 'everyone' else '<@&' + DISCORD_ROLE_ID + '>, ')
 
     data = {
-        "content": mention_str,
-        # "embeds": [
-        #     {
-        #         "author": {
-        #             "name": TWITCH_USERNAME,
-        #             "url": "https://twitch.tv/" + TWITCH_USERNAME,
-        #             "icon_url": "https://avatar.glue-bot.xyz/twitch/" + TWITCH_USERNAME
-        #         },
-        #         "image": {
-        #             "url": "https://static-cdn.jtvnw.net/previews-ttv/live_user_" + TWITCH_USERNAME + "-720x480.jpg?cache=" + cache_buster
-        #         },
-        #         "footer": {
-        #             "text": "https://twitch.tv/" + TWITCH_USERNAME,
-        #             "icon_url": "https://i.imgur.com/p4CqTrc.png"
-        #         },
-        #         "color": 8521140
-        #     }
-        # ]
+        "content": "Hey " + mention_str + "**" + channel.string + "** just uploaded a video! Check out '" + title.string + "' here:\n" + link.get("href")
     }
 
     result = requests.post(DISCORD_WEBHOOK_URL, json = data)
-
-    try:
-        result.raise_for_status()
-    except requests.exceptions.HTTPError as err:
-        return_obj["body"] = json.dumps({ "executed": False, "error": "discord webhook returned an error. " + err, "rqid": context["aws_request_id"] })
-    else:
-        return_obj["body"] = json.dumps({ "executed": True, "rqid": context["aws_request_id"] })
+    result.raise_for_status()
 
     return return_obj
